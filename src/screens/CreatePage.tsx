de3238cbe1cbd1510e5f0a61feea8b4825a18009
AmanuelAsfaw <amanuelasfawardi@gmail.com>
2022-05-24 14:28:59 +0300

    Create Page

---
+++ src/screens/CreatePage.tsx
@@ -0,0 +1,32 @@
+import styled from "styled-components"
+import CreateEmployee from "../components/create"
+
+const CreatePage = () => {
+    const Title = styled.h1`
+        padding : 10px;
+    `
+    const Wrapper = styled.div`
+        text-align: center;
+    `
+    const CreateLink = styled.a`
+        text-decoration: none;
+        border-radius: 15px;
+        font-size: larger;
+        background-color: #929eaa;
+        color: hsl(240deg 7% 97%);
+        padding : 10px;
+    `
+    const FormWrapper = styled.div`
+        align-items: center;
+        justify-content: center;
+        display: flex;
+    `
+
+    return <Wrapper>
+        <Title>Create Employee</Title>
+        <CreateLink href="/">Back</CreateLink>
+        <CreateEmployee/>
+    </Wrapper>
+}
+
+export default CreatePage
