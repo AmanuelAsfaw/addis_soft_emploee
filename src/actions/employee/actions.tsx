82141c53a1e05b87e1319952eb997c07f471f544
AmanuelAsfaw <amanuelasfawardi@gmail.com>
2022-05-26 09:23:13 +0300

    Actions for add employee request

--- src/actions/employee/actions.tsx
+++ src/actions/employee/actions.tsx
@@ -1,31 +1,60 @@
 import {
-    FETCH_EMPLOYEE_REQUEST,
-    FETCH_EMPLOYEE_FAILURE,
-    FETCH_EMPLOYEE_SUCCESS,
-  } from "./type";
+  FETCH_EMPLOYEE_REQUEST,
+  FETCH_EMPLOYEE_FAILURE,
+  FETCH_EMPLOYEE_SUCCESS,
+  CREATE_EMPLOYEE_REQUEST,
+  CREATE_EMPLOYEE_FAILURE,
+  CREATE_EMPLOYEE_SUCCESS,
+} from "./type";
 
-  import {
-    FetchEmployeeRequest,
-    FetchEmployeeSuccess,
-    FetchEmployeeSuccessPayload,
-    FetchEmployeeFailure,
-    FetchEmployeeFailurePayload,
-  } from "./type";
-  
-  export const fetchEmployeeRequest = (): FetchEmployeeRequest => ({
-    type: FETCH_EMPLOYEE_REQUEST,
-  });
-  
-  export const fetchEmployeeSuccess = (
-    payload: FetchEmployeeSuccessPayload
-  ): FetchEmployeeSuccess => ({
-    type: FETCH_EMPLOYEE_SUCCESS,
-    payload,
-  });
-  
-  export const fetchEmployeeFailure = (
-    payload: FetchEmployeeFailurePayload
-  ): FetchEmployeeFailure => ({
-    type: FETCH_EMPLOYEE_FAILURE,
-    payload,
-  });
+import {
+  FetchEmployeeRequest,
+  FetchEmployeeSuccess,
+  FetchEmployeeSuccessPayload,
+  FetchEmployeeFailure,
+  FetchEmployeeFailurePayload,
+  AddEmployeeRequest,
+  AddEmployeeSuccessPayload,
+  AddEmployeeSuccess,
+  AddEmployeeFailurePayload,
+  AddEmployeeFailure,
+  IEmployee,
+} from "./type";
+
+export const fetchEmployeeRequest = (): FetchEmployeeRequest => ({
+  type: FETCH_EMPLOYEE_REQUEST,
+});
+
+export const fetchEmployeeSuccess = (
+  payload: FetchEmployeeSuccessPayload
+): FetchEmployeeSuccess => ({
+  type: FETCH_EMPLOYEE_SUCCESS,
+  payload,
+});
+
+export const fetchEmployeeFailure = (
+  payload: FetchEmployeeFailurePayload
+): FetchEmployeeFailure => ({
+  type: FETCH_EMPLOYEE_FAILURE,
+  payload,
+});
+
+// Add Employee request actions
+export const addEmployeeRequest = (employee: IEmployee) : AddEmployeeRequest => ({
+  type : CREATE_EMPLOYEE_REQUEST,
+  employee : employee,
+})
+
+export const addEmployeeSuccess = (
+  payload : AddEmployeeSuccessPayload
+): AddEmployeeSuccess => ({
+  type : CREATE_EMPLOYEE_SUCCESS,
+  payload,
+})
+
+export const addEmployeeFailure = (
+  payload : AddEmployeeFailurePayload
+) : AddEmployeeFailure => ({
+  type : CREATE_EMPLOYEE_FAILURE,
+  payload,
+})
