f3c97a76957203ca942180fdbec21151b8be3afd
AmanuelAsfaw <amanuelasfawardi@gmail.com>
2022-05-26 09:22:45 +0300

    Types for add employee request

--- src/actions/employee/type.tsx
+++ src/actions/employee/type.tsx
@@ -2,6 +2,10 @@ export const FETCH_EMPLOYEE_REQUEST = "FETCH_EMPLOYEE_REQUEST";
 export const FETCH_EMPLOYEE_SUCCESS = "FETCH_EMPLOYEE_SUCCESS";
 export const FETCH_EMPLOYEE_FAILURE = "FETCH_EMPLOYEE_FAILURE";
 
+export const CREATE_EMPLOYEE_REQUEST = "CREATE_EMPLOYEE_REQUEST";
+export const CREATE_EMPLOYEE_SUCCESS = "CREATE_EMPLOYEE_SUCCESS";
+export const CREATE_EMPLOYEE_FAILURE = "CREATE_EMPLOYEE_FAILURE";
+
 export interface IEmployee {
   _id: number | string;
   name: string;
@@ -14,6 +18,9 @@ export interface EmployeeState {
   pending: boolean;
   employees: IEmployee[];
   error: string | null;
+  success : boolean;
+  message : string | null;
+  addedEmployee : IEmployee | null;
 }
 
 // featch employees list data
@@ -39,7 +46,38 @@ export type FetchEmployeeFailure = {
   payload: FetchEmployeeFailurePayload;
 };
 
+// Add employee to database
+export interface AddEmployeeSuccessPayload {
+  success : boolean | false;
+  addedEmployee : IEmployee | null;
+  message : string | null;
+}
+
+export interface AddEmployeeFailurePayload {
+  error : string | Object | null;
+  message : string | null;
+  success : boolean | null | false;
+}
+
+export interface AddEmployeeRequest {
+  type : typeof CREATE_EMPLOYEE_REQUEST;
+  employee : IEmployee;
+}
+
+export type AddEmployeeSuccess = {
+  type : typeof CREATE_EMPLOYEE_SUCCESS;
+  payload : AddEmployeeSuccessPayload;
+}
+
+export type AddEmployeeFailure = {
+  type : typeof CREATE_EMPLOYEE_FAILURE;
+  payload : AddEmployeeFailurePayload;
+}
+
 export type EmployeeActions =
   | FetchEmployeeRequest
   | FetchEmployeeSuccess
-  | FetchEmployeeFailure;
+  | FetchEmployeeFailure
+  | AddEmployeeRequest
+  | AddEmployeeSuccess
+  | AddEmployeeFailure;
