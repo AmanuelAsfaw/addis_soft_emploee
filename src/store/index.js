c3aca6641175c834c43b02c03db8c0444f26deb9
AmanuelAsfaw <amanuelasfawardi@gmail.com>
2022-05-26 09:19:21 +0300

    Add DevTools for redux

--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,18 @@ import { createStore, applyMiddleware } from "redux";
 import createSagaMiddleware from "redux-saga";
 import logger from "redux-logger";
 
+import { composeWithDevTools } from 'redux-devtools-extension';
+
 import rootReducer from "../reducers";
 import {rootSaga} from "../sagas";
 
 const sagaMiddleware = createSagaMiddleware();
 
-const store = createStore(rootReducer, applyMiddleware(sagaMiddleware, logger))
+const store = createStore(
+    rootReducer, 
+    composeWithDevTools(
+        applyMiddleware(sagaMiddleware, logger)
+    ))
 
 sagaMiddleware.run(rootSaga)
 
